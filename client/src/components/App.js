39d2de7d57f3f4d6cdb0d93f9e307d33b16cf30a
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2022-07-22 21:00:02 +0300

    Refactor code to be readable

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,9 @@
 import TokenList from "./TokenList";
-import Product from "./Product";
 import ProductList from "./ProductList";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Ubeswap from "./Ubeswap";
-import Deposit from "./Deposit";
-import Withdraw from "./Withdraw";
-import { useState } from "react";
+
 
 function App() {
   
@@ -18,7 +15,7 @@ function App() {
         <Route path="/portfolio" element={<ProductList  />} />
         <Route path="/portfolio/tokens" element={<TokenList />} />
         <Route path="/portfolio/ubeswap" element={<Ubeswap  />} />
-      </Routes>{" "}
+      </Routes>
     </Router>
     
     </>
