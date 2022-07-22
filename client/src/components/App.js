c780cd04c7aeef43b1c5ad242d76473502afc6fa
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2022-07-22 13:36:13 +0300

    Create a Deposit component for users to deposit their tokens

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,12 +4,13 @@ import ProductList from "./ProductList";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Ubeswap from "./Ubeswap";
+import Deposit from "./Deposit";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Ubeswap />} />{" "}
+        <Route path="/" element={<Deposit />} />
         <Route path="/portfolio" element={<ProductList />} />
         <Route path="/portfolio/:id" element={<TokenList />} />
       </Routes>{" "}
